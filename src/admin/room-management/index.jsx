c76a17539893f7fcf78ba57f42a85e4bc415e7bb
Rauf Dimaampao <abdulraufdimaampao@ADimaampao-EDP-100-365.local>
2022-03-18 20:45:12 +0800

    fixed report

--- src/admin/room-management/index.jsx
+++ src/admin/room-management/index.jsx
@@ -4,15 +4,19 @@ import { Routes as Switch, Route, Navigate } from "react-router-dom";
 import RoomTypeList from "./views/RoomTypeList";
 import RoomTypeDetails from "./views/RoomTypeDetails";
 import RoomTypeCreate from "./views/RoomTypeCreate";
+import { WindowTitle } from "./../components/WindowTitle/WindowTitle";
 
 const Component = () => {
   return (
-    <Switch>
-      <Route exact path="/" element={<RoomTypeList />} />
-      <Route exact path="/add" element={<RoomTypeCreate />} />
-      <Route exact path=":id" element={<RoomTypeDetails />} />
-      <Route exact path="*" element={<Navigate from="*" to="/admin" />} />
-    </Switch>
+    <>
+      <WindowTitle title={"Rooms"} />
+      <Switch>
+        <Route exact path="/" element={<RoomTypeList />} />
+        <Route exact path="/add" element={<RoomTypeCreate />} />
+        <Route exact path=":id" element={<RoomTypeDetails />} />
+        <Route exact path="*" element={<Navigate from="*" to="/admin" />} />
+      </Switch>
+    </>
   );
 };
 
